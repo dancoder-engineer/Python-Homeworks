3e0eb10e976ef771c105d289892437f7f97acd0a
Dan Seminara <japaneseforgaming@gmail.com>
2022-12-02 16:01:33 -0500

    API

---
+++ 06 - API/API.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+urlstem = "http://py4e-data.dr-chuck.net/json?"
+#address = "South Federal University"
+address = "Smolensk State University"
+
+address = input("Enter location: ")
+
+url = urlstem + urllib.parse.urlencode({'address': address, 'key': 42})
+print("Retrieving " + url)
+
+rawJson = urllib.request.urlopen(url).read().decode()
+js = json.loads(rawJson)
+
+print("Retrieved " + str(len(rawJson)) + " characters")
+print("Place id " + js['results'][0]["place_id"])
